50eb10c1a7ae433e7e8b6bd7c61a148dbde49574
Serhiy Bardysh <sbardysh@tutanota.com>
2020-07-15 01:21:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -26,9 +26,9 @@ class Triangle
     end
 
 
-  class TriangleError < StandardError
-    def message
-      "That's no triangle. Of that I'm sure."
-    end
+    class TriangleError < StandardError
+      def message
+        "That's no triangle. Of that I'm sure."
+      end
   
 end

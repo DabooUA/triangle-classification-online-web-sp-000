f371f184456d223f19bb6f6a3c3500c92a2e5fd6
Serhiy Bardysh <sbardysh@tutanota.com>
2020-07-15 01:17:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,30 @@
 class Triangle
 
-  attr_accessor :equilateral, :isosceles, :scalene
+  attr_reader :hypotenuse, :adjacent, :opposite, :sides
+end 	
+  def initialize(hypotenuse, adjacent, opposite)
+    @hypotenuse = hypotenuse
+    @adjacent = adjacent
+    @opposite = opposite
+    @sides = [hypotenuse, adjacent, opposite].sort
+  end
 
-  def initialize (equilateral:, isosceles:, scalene:)
-    @equilateral = equilateral
-    @isosceles = isosceles
-    @scalene = scalene
+  def kind
+    if invalid_triangle?
+      raise TriangleError
+    elsif sides.uniq.length == 1
+      :equilateral
+    elsif sides.uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
 
+  def invalid_triangle?
+    sides.any? { |side| side <= 0 } || sides[0] + sides[1] <= sides[2]
+  end
+  
   end
 
     class TriangleError < StandardError
